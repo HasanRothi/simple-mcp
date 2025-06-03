ea770cb683b6a739b1e98e6917a4a8fd10edeb43
rothi <hasanrothi99@gmail.com>
2025-06-03 11:52:47 +0600

    read env for server_url

--- client.py
+++ client.py
@@ -2,17 +2,24 @@ import asyncio
 from fastmcp import Client
 import json
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables
+
+MCP_SERVER_BASE_URL = os.environ.get('MCP_SERVER_BASE_URL', '')
+
 
 async def main():
-    # Initialize the client with the path to the server script
-    client = Client("http://127.0.0.1:9000/mcp")
+    client = Client(MCP_SERVER_BASE_URL + "/mcp")
+
     async with client:
-        # Call the 'greet' tool with a parameter
         companies = await client.call_tool("companies", {"country": ""})
         if not companies:
             return
         company_list = json.loads(companies[0].text)
         print(company_list)
+
         models = await client.call_tool("models", {"company_list": company_list})
         if not models:
             return
